d848990c53f250eb48cb46444bf3390bf4aea07e
FineIsCool <143910642+FineIsCool@users.noreply.github.com>
2025-03-30 00:52:59 +0100

    api.lua

---
+++ api.lua
@@ -0,0 +1,14 @@
+{
+  "6f93f2271ad19cd445247018d23a08cbf5caeab93cb0d64049bf0d89e0c737f9": {
+    "boosting_since": 1741458437,
+    "color": [256, 256, 256]
+  },
+  "813f4ccbbf29359b99363ef726ea07f8ecf2af55d89ba8cca10014f26eaca0e5": {
+    "boosting_since": 1736162112,
+    "color": [256, 256, 256]
+  },
+  "9ed65a7a888695c26259d359fbaad62815a8ba4d87d0921f4c58c253a1c68473": {
+    "boosting_since": 1732069640,
+    "color": [256, 256, 256]
+  }
+}
